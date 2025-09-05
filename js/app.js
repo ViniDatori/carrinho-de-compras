3a623d048cb64bcbf35238147ab76f2f7742459d
Vinícius Datori <vinidatori@gmail.com>
2025-09-05 15:36:37 -0300

    Otimização de lista e função limpar carrinho + Definição básica da função adicionar.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
-let carrinho = [];
+// Definição de lista tipo "Map" para facilitar o controle dos itens e suas quantidades:
+
+let carrinho = new Map();
+carrinho.set("Fone", parseInt(0));
+carrinho.set("Celular", parseInt(0));
+carrinho.set("Óculos", parseInt(0));
 
 // Função para adicionar itens ao carrinho:
 
 function adicionar() {
 
+    let produtoSelecionado = document.getElementById("produto");
+    let quantidadeProduto = parseInt(document.getElementById("quantidade").value);
+
+    if (produtoSelecionado.value == "Fone de ouvido - R$100") {
+        carrinho.set("Fone", carrinho.get("Fone") + parseInt(quantidadeProduto));
+    } else {
+        if (produtoSelecionado.value == "Celular - R$1400") {
+            carrinho.set("Celular", carrinho.get("Celular") + parseInt(quantidadeProduto));
+        } else {
+            if (produtoSelecionado.value == "Oculus VR - R$5000") {
+                carrinho.set("Óculos", carrinho.get("Óculos") + parseInt(quantidadeProduto));
+            }
+        }
+    }
+    console.log(carrinho);
 }
 
 
 // Função para limpar o carrinho:
 
 function limpar() {
-    carrinho = [];
+    carrinho.clear();
     let produtos = document.getElementById("lista-produtos");
     produtos.innerHTML = "-";
     let valorTotal = document.getElementById("valor-total");
     valorTotal.innerHTML = "R$0";
+    console.log(carrinho);
 }
